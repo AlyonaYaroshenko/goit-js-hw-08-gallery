4398b80dcacac7ee37601a3121829d367e37d286
AlyonaYaroshenko <alyona.yaroshenko03@gmail.com>
2021-05-09 15:47:25 +0300

    added opening original pic on click

--- src/gallery.js
+++ src/gallery.js
@@ -39,23 +39,52 @@ function createGallery(gallery) {
 const openModalBtn = document.querySelector('.lightbox');
 const closeModalBtn = document.querySelector('[data-action="close-lightbox"]');
 const backdrop = document.querySelector('.lightbox__overlay');
+const preventMisclick = document.querySelector('.gallery__link');
 
 galleryContainer.addEventListener('click', onGalleryContainerClick);
 closeModalBtn.addEventListener('click', onCloseModal);
 backdrop.addEventListener('click', onBackdropCklick);
+preventMisclick.addEventListener('click', onLinkClick);
 
 
 
 function onGalleryContainerClick(event) {
   event.preventDefault();
- window.addEventListener('keydown', onEscKeyPress);
+  window.addEventListener('keydown', onEscKeyPress);
+  if (event.target.nodeName === 'IMG') {
     openModalBtn.classList.add('is-open');
+    openModalBtn.querySelector('.lightbox__image').src = event.target.dataset.source;
+    openModalBtn.querySelector('.lightbox__image').alt = event.target.alt;
+  }
 };
+
+function onLinkClick(event) {
+  if (event.currentTarget !== event.target) {
+    return;
+  }
+}
+
+// if (e.target.nodeName === "IMG") {
+  //   refs.lightBox.classList.add("is-open");
+  //   const originalImageUrl = e.target.dataset.source;
+  //   refs.originalImage.src = originalImageUrl;
+  //   refs.originalImage.alt = e.target.alt;
+  // }
+
+  // if(e.target.nodeName === 'IMG') {
+  //   openModalBtn.classList.add('is-open');
+  //   openModalBtn.querySelector('.lightbox__image').src = e.target.src;
+  //   openModalBtn.querySelector('.lightbox__image').alt = e.target.alt;
+  // }
 
 function onCloseModal() {
     window.removeEventListener('keydown', onEscKeyPress);
     openModalBtn.classList.remove('is-open');
 }
+
+// refs.originalImage.src = "";
+  // refs.originalImage.alt = "";
+  // refs.lightBox.classList.remove("is-open");
 
 function onBackdropCklick(event) {
     // console.log(event.currentTarget);
@@ -68,7 +97,7 @@ function onBackdropCklick(event) {
 function onEscKeyPress(event) {
   const ESC_KEY_CODE = 'Escape';
   if (event.code === ESC_KEY_CODE) {
-    onCloseModal();
+  onCloseModal();
   }
 }
 
